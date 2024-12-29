be40c2ff0fc6d98630b1281069fa54339408df06
browncatt <gaellascott@gmail.com>
2024-12-29 17:26:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
+    });
 
-         // Optional: Close the menu when a link is clicked
+    // Optional: Close the menu when a link is clicked
     document.querySelectorAll('.nav-links a').forEach(link => {
         link.addEventListener('click', () => {
             navLinks.classList.remove('active');
+        });
     });
 });
+
 
